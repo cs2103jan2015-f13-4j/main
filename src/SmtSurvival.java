6aa9eb337447658cde9a6367610c60ce8be9bb5c
TS <ts_neo89@hotmail.com>
2015-03-04 15:43:24 +0800

    Created Composite GUI Form

---
+++ src/SmtSurvival.java
@@ -0,0 +1,29 @@
+import org.eclipse.swt.events.DisposeEvent;
+import org.eclipse.swt.events.DisposeListener;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.forms.widgets.FormToolkit;
+
+
+public class SmtSurvival extends Composite {
+
+	private final FormToolkit toolkit = new FormToolkit(Display.getCurrent());
+
+	/**
+	 * Create the composite.
+	 * @param parent
+	 * @param style
+	 */
+	public SmtSurvival(Composite parent, int style) {
+		super(parent, style);
+		addDisposeListener(new DisposeListener() {
+			public void widgetDisposed(DisposeEvent e) {
+				toolkit.dispose();
+			}
+		});
+		toolkit.adapt(this);
+		toolkit.paintBordersFor(this);
+
+	}
+
+}
