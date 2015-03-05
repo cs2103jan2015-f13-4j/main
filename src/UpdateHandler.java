2cc8dd94e9ff335bfe418c22c5c8f9ce07e830d1
TS <ts_neo89@hotmail.com>
2015-03-05 23:13:51 +0800

    Created UpdateHandler Class

---
+++ src/UpdateHandler.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+
+public class UpdateHandler {
+
+	public static Task process(String commandInput){
+		
+		
+		
+		//dummy
+		return new Task(1, "", new Date(), new Date());
+	}
+}
