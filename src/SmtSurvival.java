700b7261d80a4a56e220ba329b9520a767c1ad3f
Baoyi <huangbaoyi92@hotmail.com>
2015-03-06 10:40:28 +0800

    SSH tesing

--- src/SmtSurvival.java
+++ src/SmtSurvival.java
@@ -16,6 +16,7 @@ public class SmtSurvival extends Composite {
 	 */
 	public SmtSurvival(Composite parent, int style) {
 		super(parent, style);
+		System.out.println("Testing");
 		addDisposeListener(new DisposeListener() {
 			public void widgetDisposed(DisposeEvent e) {
 				toolkit.dispose();
