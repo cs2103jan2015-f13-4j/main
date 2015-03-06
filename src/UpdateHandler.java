0bacada6d90319c24d8bb612600a0810038cd380
TS <ts_neo89@hotmail.com>
2015-03-06 11:44:34 +0800

    Create FileHandler class, edit updateHandler class and amended Task class

--- src/UpdateHandler.java
+++ src/UpdateHandler.java
@@ -4,13 +4,36 @@ import java.util.Date;
 
 public class UpdateHandler {
 
+	//private static final int 
+	
 	public static String executeUpdate(String[] commandInput, ArrayList<Task> listTask){
 		
 		if(commandInput.length == 1){
 			return String.format(MessageList.MESSAGE_INVALID_ARGUMENT, "Delete");
 		}
 		
+		if(!isStringAnInteger(commandInput[1])){
+			//return String.for
+		}
+		
 		//dummy
 		return "";
+	}
+	
+	/**
+	 * This method check if the given string can be converted to integer
+	 * 
+	 * @param inputStr
+	 *            the string to be converted
+	 * @return true if can be converted, else false
+	 */
+	private static boolean isStringAnInteger(String inputStr) {
+		try {
+			Integer.parseInt(inputStr);
+		} catch (NumberFormatException e) {
+			return false;
+		}
+
+		return true;
 	}
 }
