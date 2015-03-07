f00d120d270b6bad380ed1984adfbe1b644f0018
TS <ts_neo89@hotmail.com>
2015-03-07 09:35:08 +0800

    created keyword sets which to determine what are the keywords

---
+++ src/KeywordType.java
@@ -0,0 +1,37 @@
+
+public class KeywordType {
+	public enum List_Keywords{
+		FROM, BY, TASKDESC, TASKSTART, TASKEND, FIELD
+	}
+	
+	public static List_Keywords getKeyword(String keywordTypeString){
+		if (keywordTypeString == null) {
+			return List_Keywords.FIELD;
+		}
+
+		switch(keywordTypeString.toLowerCase()){
+		case "from":
+			return List_Keywords.FROM;
+		case "by":
+			return List_Keywords.BY;
+		case "taskdesc":
+			return List_Keywords.TASKDESC;
+		case "taskstart":
+			return List_Keywords.TASKSTART;
+		case "taskend":
+			return List_Keywords.TASKEND;
+		default:
+			return List_Keywords.FIELD;
+		}
+	}
+	
+	public static boolean contains(String input){
+		for (List_Keywords aKey : List_Keywords.values()) {
+	        if (aKey.name().equals(input)) {
+	            return true;
+	        }
+	    }
+
+	    return false;
+	}
+}
