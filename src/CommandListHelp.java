b3454a3d699ab02e2eb0424155008133c8c1bc96
Baoyi <huangbaoyi92@hotmail.com>
2015-03-07 14:31:39 +0800

    Create a CommandListHelp class

--- src/CommandListHelp.java
+++ src/CommandListHelp.java
@@ -14,20 +14,28 @@ public class CommandListHelp {
 			switch (commandListHelp_TypeString[0].toLowerCase()) {
 			case "add": 
 				return CommandListHelp_Type.ADD;
+				
 			case "delete": 
 				return CommandListHelp_Type.DELETE;
+				
 			case "display": 
 				return CommandListHelp_Type.DISPLAY;
+				
 			case "update": 
 				return CommandListHelp_Type.UPDATE;
+				
 			case "clear": 
 				return CommandListHelp_Type.CLEAR;
+				
 			case "exit": 
 				return CommandListHelp_Type.EXIT;
+			
 			case "search": 
 				return CommandListHelp_Type.SEARCH;
+			
 			case "sort": 
 				return CommandListHelp_Type.SORT;
+			
 			default: 
 				return CommandListHelp_Type.INVALID;
 			}
