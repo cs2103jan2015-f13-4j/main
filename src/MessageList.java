2f5efbf0d09f583f236fd7985163b26cd59445f7
Baoyi <huangbaoyi92@hotmail.com>
2015-03-08 19:31:53 +0800

    make necessary amendment for MessageList for HelpCommandListHandler

--- src/MessageList.java
+++ src/MessageList.java
@@ -29,18 +29,18 @@ public class MessageList {
 	/**
 	 * This message are for HelpCommandList class
 	 */
-	public static final String MESSAGE_HELP = "1) Add<Task Name> <Desc>\n 2) Remove<Task #>\n 3) Update<Task #><Desc>\n 4) Display<Task #>\n 5) Search <Task #>\n 6)Switchview <tabname>\n 7) Block <Task #><period>\n 8) Unblock <Task #><Period>";
-	public static final String MESSAGE_ADD_HELP = "Add<Task Name> <Desc>";
-	public static final String MESSAGE_DELETE_HELP = "Remove<Task #>";
-	public static final String MESSAGE_UPDATE_HELP = "Update<Task #> <Desc>";
-	public static final String MESSAGE_DISPLAY_HELP = "Display<Task #>";
-	public static final String MESSAGE_SEARCH_HELP = "Search<Task #>";
+	public static final String MESSAGE_HELP = "1) Add <Desc>\n 2) Remove<Task #>\n 3) 1. Update<Task #> TaskDes <description>\n2.Update<Task #> By <date/day>\n 4) Display<Task #>\n 5) Search <Task #>\n 6)Switchview <tabname>\n 7) Block <Task #><period>\n 8) Unblock <Task #><Period>";
+	public static final String MESSAGE_ADD_HELP = "Add <Desc>";
+	public static final String MESSAGE_DELETE_HELP = "Delete <Task #>";
+	public static final String MESSAGE_UPDATE_HELP = "1. Update <Task #> TaskDesc <description>\n2. Update <Task #> By <date/day>";
+	public static final String MESSAGE_DISPLAY_HELP = "Display";
+	public static final String MESSAGE_SEARCH_HELP = "Search <Task #>";
 	public static final String MESSAGE_SORT_HELP = "Sort";
 	public static final String MESSAGE_CLEAR_HELP = "Clear";
 	public static final String MESSAGE_EXIT_HELP = "Exit";
-	public static final String MESSAGE_SWITCHVIEW_HELP = "Switchview<tabname> <Desc>";
-	public static final String MESSAGE_BLOCK_HELP = "Block <Task #> <Desc>";
-	public static final String MESSAGE_UNBLOCK_HELP = "Unblock<Task Name> <Desc>";
+	public static final String MESSAGE_SWITCHVIEW_HELP = "Switchview <tabname> <Desc>";
+	public static final String MESSAGE_BLOCK_HELP = "Block <Task #> <Date>";
+	public static final String MESSAGE_UNBLOCK_HELP = "Unblock <Date>";
 	public static final String MESSAGE_INVAILD = "Please enter a vaild command.";
 	
 	/**
