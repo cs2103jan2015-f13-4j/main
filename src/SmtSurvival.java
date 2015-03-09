3c88fa1b63f4291f9ea1f181dfef2afcdf17e815
Baoyi <huangbaoyi92@hotmail.com>
2015-03-09 20:15:36 +0800

    rearrange de codes in AddHandler switch

--- src/SmtSurvival.java
+++ src/SmtSurvival.java
@@ -1,39 +1,21 @@
-import java.awt.TextField;
 
+
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.DisposeEvent;
 import org.eclipse.swt.events.DisposeListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.forms.widgets.FormToolkit;
-import org.eclipse.swt.custom.StackLayout;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.layout.FillLayout;
-import org.eclipse.swt.layout.FormLayout;
-
-import swing2swt.layout.BoxLayout;
-
-import org.eclipse.swt.custom.SashForm;
-import javax.swing.JTabbedPane;
-import javax.swing.JSplitPane;
-import javax.swing.JPanel;
-import org.eclipse.swt.custom.StyledText;
-import org.eclipse.jface.text.TextViewer;
-import javax.swing.JTextPane;
 
 
 public class SmtSurvival extends Composite {
 
 	private final FormToolkit toolkit = new FormToolkit(Display.getCurrent());
-	private Text text;
-	private Text text_1;
-	private Text txtSmartManagementTool;
-	private SashForm sashForm;
-	private Label lblCommands;
 	private Text txtSmartManagementTool_1;
 	private Text text_2;
 
@@ -46,12 +28,12 @@ public class SmtSurvival extends Composite {
 		super(parent, style);
 	
 		Group group = new Group(this, SWT.NONE);
-		group.setBounds(10, 10, 597, 417);
+		group.setBounds(10, 10, 643, 417);
 		toolkit.adapt(group);
 		toolkit.paintBordersFor(group);
+		group.setLayout(new FillLayout(SWT.HORIZONTAL));
 		
 		Composite composite = new Composite(group, SWT.NONE);
-		composite.setBounds(0, 10, 157, 22);
 		toolkit.adapt(composite);
 		toolkit.paintBordersFor(composite);
 		
@@ -60,19 +42,23 @@ public class SmtSurvival extends Composite {
 		txtSmartManagementTool_1.setBounds(0, 1, 157, 21);
 		toolkit.adapt(txtSmartManagementTool_1, true, true);
 		
-		Composite composite_1 = new Composite(group, SWT.NONE);
-		composite_1.setBounds(10, 363, 313, 32);
-		toolkit.adapt(composite_1);
-		toolkit.paintBordersFor(composite_1);
-		
-		Label lblCommands_1 = new Label(composite_1, SWT.NONE);
-		lblCommands_1.setBounds(0, 10, 68, 15);
+		Label lblCommands_1 = new Label(composite, SWT.NONE);
+		lblCommands_1.setBounds(10, 371, 275, 15);
 		toolkit.adapt(lblCommands_1, true, true);
 		lblCommands_1.setText("Commands :");
 		
-		text_2 = new Text(composite_1, SWT.BORDER);
-		text_2.setBounds(74, 7, 233, 21);
+		text_2 = new Text(composite, SWT.BORDER);
+		text_2.setBounds(84, 368, 201, 21);
 		toolkit.adapt(text_2, true, true);
+		
+		StyledText styledText = new StyledText(composite, SWT.BORDER);
+		styledText.setBounds(10, 28, 298, 334);
+		toolkit.adapt(styledText);
+		toolkit.paintBordersFor(styledText);
+		
+		Composite composite_1 = new Composite(group, SWT.NONE);
+		toolkit.adapt(composite_1);
+		toolkit.paintBordersFor(composite_1);
 		addDisposeListener(new DisposeListener() {
 			public void widgetDisposed(DisposeEvent e) {
 				toolkit.dispose();
