100d444888255bd44677b041961285383c8438c7
Shuna <shunaa.lim@hotmail.com>
2015-03-11 12:38:22 +0800

    Type in coding to retrieve text from the text box to the tab box

--- src/SmtSurvival.java
+++ src/SmtSurvival.java
@@ -18,114 +18,139 @@ import org.eclipse.swt.layout.GridData;
 //import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
+import org.eclipse.swt.events.KeyAdapter;
+import org.eclipse.swt.events.KeyEvent;
+
 //import org.eclipse.wb.swt.SWTResourceManager;
 
 public class SmtSurvival extends Composite {
 
 	private final FormToolkit toolkit = new FormToolkit(Display.getCurrent());
 	private Text text;
-	private Text text_1;
+	private Text cmdTextBox;
+	private TabFolder displayTaskFolder;
+	private TabItem tbtmToday;
+	private TabItem tbtmSchedule;
+	private TabItem tbtmPending;
+	private TabItem tbtmTodo;
+	private TabItem tbtmMisc;
+	private Label lblToday;
 
 	/**
 	 * Create the composite.
+	 * 
 	 * @param parent
 	 * @param style
 	 */
 	public SmtSurvival(Composite parent, int style) {
 		super(parent, SWT.BORDER | SWT.NO_BACKGROUND);
-		//setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
+		// setBackground(SWTResourceManager.getColor(SWT.COLOR_WHITE));
 		addDisposeListener(new DisposeListener() {
 			public void widgetDisposed(DisposeEvent e) {
 				toolkit.dispose();
 			}
 		});
-		
+
 		toolkit.adapt(this);
 		toolkit.paintBordersFor(this);
 		setLayout(new GridLayout(2, false));
-		
+
 		Group grpOutput = new Group(this, SWT.NONE);
-		GridData gd_grpOutput = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		GridData gd_grpOutput = new GridData(SWT.LEFT, SWT.CENTER, false,
+				false, 1, 1);
 		gd_grpOutput.heightHint = 381;
 		gd_grpOutput.widthHint = 424;
 		grpOutput.setLayoutData(gd_grpOutput);
 		grpOutput.setText("Output");
 		toolkit.adapt(grpOutput);
 		toolkit.paintBordersFor(grpOutput);
-		
+
 		Composite composite = new Composite(grpOutput, SWT.NONE);
 		composite.setBounds(10, 22, 410, 372);
 		toolkit.adapt(composite);
 		toolkit.paintBordersFor(composite);
-		
+
 		text = new Text(composite, SWT.BORDER);
 		text.setBounds(0, 0, 410, 372);
 		toolkit.adapt(text, true, true);
-		
+
 		Group group = new Group(this, SWT.NONE);
-		GridData gd_group = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 2);
+		GridData gd_group = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1,
+				2);
 		gd_group.heightHint = 473;
 		gd_group.widthHint = 424;
 		group.setLayoutData(gd_group);
 		toolkit.adapt(group);
 		toolkit.paintBordersFor(group);
-		
-		TabFolder tabFolder = new TabFolder(group, SWT.NONE);
-		tabFolder.setBounds(0, 10, 430, 476);
-		toolkit.adapt(tabFolder);
-		toolkit.paintBordersFor(tabFolder);
-		
-		TabItem tbtmToday = new TabItem(tabFolder, SWT.NONE);
+
+		displayTaskFolder = new TabFolder(group, SWT.NONE);
+		displayTaskFolder.setBounds(0, 10, 430, 476);
+		toolkit.adapt(displayTaskFolder);
+		toolkit.paintBordersFor(displayTaskFolder);
+
+		tbtmToday = new TabItem(displayTaskFolder, SWT.NONE);
 		tbtmToday.setText("Today");
-		Label lblNewLabel1 = new Label(tabFolder, SWT.NONE);
-		tbtmToday.setControl(lblNewLabel1);
-		lblNewLabel1.setText("You are now in the today tab");
-		
-		TabItem tbtmSchedule = new TabItem(tabFolder, SWT.NONE);
+		lblToday = new Label(displayTaskFolder, SWT.NONE);
+		tbtmToday.setControl(lblToday);
+		lblToday.setText("You are now in the today tab");
+
+		tbtmSchedule = new TabItem(displayTaskFolder, SWT.NONE);
 		tbtmSchedule.setText("Schedule");
-		
-		TabItem tbtmPending = new TabItem(tabFolder, SWT.NONE);
+
+		tbtmPending = new TabItem(displayTaskFolder, SWT.NONE);
 		tbtmPending.setText("Pending");
-		
-		TabItem tbtmTodo = new TabItem(tabFolder, SWT.NONE);
+
+		tbtmTodo = new TabItem(displayTaskFolder, SWT.NONE);
 		tbtmTodo.setText("To-Do");
-		
-		TabItem tbtmMisc = new TabItem(tabFolder, SWT.NONE);
+
+		tbtmMisc = new TabItem(displayTaskFolder, SWT.NONE);
 		tbtmMisc.setText("Misc");
-		
+
 		Composite composite_1 = new Composite(this, SWT.NONE);
-		GridData gd_composite_1 = new GridData(SWT.LEFT, SWT.CENTER, false, false, 1, 1);
+		GridData gd_composite_1 = new GridData(SWT.LEFT, SWT.CENTER, false,
+				false, 1, 1);
 		gd_composite_1.heightHint = 83;
 		gd_composite_1.widthHint = 429;
 		composite_1.setLayoutData(gd_composite_1);
 		toolkit.adapt(composite_1);
 		toolkit.paintBordersFor(composite_1);
-		
+
 		Label lblCommand = new Label(composite_1, SWT.NONE);
 		lblCommand.setBounds(10, 10, 84, 20);
 		toolkit.adapt(lblCommand, true, true);
 		lblCommand.setText("Command :");
-		
-		text_1 = new Text(composite_1, SWT.BORDER);
-		text_1.setBounds(100, 10, 319, 63);
-		toolkit.adapt(text_1, true, true);
+
+		cmdTextBox = new Text(composite_1, SWT.BORDER);
+		cmdTextBox.addKeyListener(new KeyAdapter() {
+
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.keyCode == SWT.CR || e.keyCode == SWT.KEYPAD_CR) {
+					displayTaskFolder.setSelection(tbtmToday);
+					lblToday.setText(cmdTextBox.getText());
+				}
+			}
+		});
+		cmdTextBox.setBounds(100, 10, 319, 63);
+		toolkit.adapt(cmdTextBox, true, true);
 	}
-	
-	public static void main(String[] args){
-		
-	    Display display = new Display();
-	    Shell shell = new Shell(display);
-	    shell.setText("Smart Management Tool");
-	    
-	    shell.open();
-	    
-	    SmtSurvival Smt = new SmtSurvival(shell, SWT.NONE);
-	    
-	    Smt.pack();
-	    shell.pack();
-	    
-	    while(!shell.isDisposed()){
-	        if(!display.readAndDispatch()) display.sleep();
-	    }
+
+	public static void main(String[] args) {
+
+		Display display = new Display();
+		Shell shell = new Shell(display);
+		shell.setText("Smart Management Tool");
+
+		shell.open();
+
+		SmtSurvival Smt = new SmtSurvival(shell, SWT.NONE);
+
+		Smt.pack();
+		shell.pack();
+
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
+				display.sleep();
+		}
 	}
 }
