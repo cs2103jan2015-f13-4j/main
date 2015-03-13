b52b3faaf5e1cda8606ec6c5beae77223851e3e5
Shuna <shunaa.lim@hotmail.com>
2015-03-13 21:19:40 +0800

    Amendments made to SmtSurvival class label display for Schedule tab

--- src/SmtSurvival.java
+++ src/SmtSurvival.java
@@ -148,7 +148,7 @@ public class SmtSurvival extends Composite {
 		}
 		else if(displayTaskFolder.getSelection()[0].equals(tbtmSchedule)){
 			tbtmSchedule.setControl(lblDisplay);
-			lblDisplay.setText("You are now in the Schedule tab");
+			lblDisplay.setText(controller.commandExecution("display schedule"));
 		}
 		else if(displayTaskFolder.getSelection()[0].equals(tbtmToday)){
 			tbtmToday.setControl(lblDisplay);
