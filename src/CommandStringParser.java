3cbd3aea24ddeaad71ae240798d6be608e38afb3
TS <ts_neo89@hotmail.com>
2015-03-15 12:51:55 +0800

    added descriptions

--- src/CommandStringParser.java
+++ src/CommandStringParser.java
@@ -1,9 +1,19 @@
 import java.util.ArrayList;
 
-
+/**
+ * This class allows the command in a line to break into keys and fields.
+ * It will returned a command type and also the referenced list of keyfields
+ * @author A0111935L
+ *
+ */
 public class CommandStringParser {
 	
-	
+	/**
+	 * This processString will process the commands entered by the user
+	 * @param input command in string
+	 * @param keyFieldsList a list of keys and fields and will be referenced back to the caller
+	 * @return Command Type
+	 */
 	public static CommandType.Command_Types processString(String input, ArrayList<KeyFieldPair> keyFieldsList){
 		if(input == null || input.equals("")){
 			return CommandType.Command_Types.INVALID;
@@ -21,6 +31,12 @@ public class CommandStringParser {
 		return command;
 	}
 
+	/**
+	 * This method generate the command which has broken up into words and process it.
+	 * The key and fields will be referened back to the caller
+	 * @param keyFieldsList a list of key and fields
+	 * @param inputCmd the blocks of word
+	 */
 	private static void generateKeykeyFieldPair(ArrayList<KeyFieldPair> keyFieldsList,
 			String[] inputCmd) {
 		String key = inputCmd[0];
