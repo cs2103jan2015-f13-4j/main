0430bc145a7a5fa63a7b9c88e92e6c27e8a9d6eb
wq <wq@project.com>
2015-03-21 15:44:33 +0800

    Updated into logic package.

---
+++ src/logic/DisplayHandler.java
@@ -0,0 +1,152 @@
+package logic;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+
+import parser.DateParser;
+import utility.CommandType;
+import utility.IndicatorMessagePair;
+import utility.KeywordType;
+import utility.MessageList;
+import data.Data;
+import data.Task;
+
+
+public class DisplayHandler {
+
+
+	private static String displayTaskDetails(ArrayList<Task> displayTasksList) {
+		String taskDetails = "";
+		for (int i = 0; i < displayTasksList.size(); i++) {
+			taskDetails += displayTasksList.get(i).toString() +"\n";
+		}
+		return taskDetails;
+	}
+	
+	
+	
+	
+	//===========================================================================================
+	//===========================================================================================
+	
+	
+	public static String executeDisplay(HashMap<String, String> keyFieldsList, Data smtData) {
+
+		int numItemExpected = 2;
+		
+		if(keyFieldsList == null || keyFieldsList.isEmpty()) {
+			return MessageList.MESSAGE_NULL;
+		}
+		
+		if(smtData == null || smtData.getListTask().isEmpty()) {
+			return MessageList.MESSAGE_NO_TASK_IN_LIST;
+		}
+		
+		if(keyFieldsList.size() != numItemExpected) {
+			return String.format(MessageList.MESSAGE_INVALID_ARGUMENT, "Display");
+		}
+		
+		return displayContents(keyFieldsList, smtData);	
+	}
+	
+	private static String displayContents(HashMap<String, String> keyFieldsList, Data smtData) {
+		
+		IndicatorMessagePair indicMsg = new IndicatorMessagePair();
+		KeywordType.List_Keywords getKey;
+		ArrayList<Task> displayTasksList = new ArrayList<Task>();
+	
+		keyFieldsList.remove(CommandType.Command_Types.DISPLAY.name());
+		for (String key : keyFieldsList.keySet()){
+			getKey = KeywordType.getKeyword(key);
+			
+			switch(getKey) {
+			case SCHEDULE:
+				indicMsg = displaySchedule(keyFieldsList, smtData, displayTasksList);
+				break;
+			case TODAY:
+				indicMsg = displayTodayTasks(keyFieldsList, smtData, displayTasksList);
+				break;
+			case TODO:
+				indicMsg = displayCompletedTasks(keyFieldsList, smtData, displayTasksList);
+				break;
+			case PENDING:
+				indicMsg = displayNotCompletedTasks(keyFieldsList, smtData, displayTasksList);
+				break;
+			default:
+				return String.format(MessageList.MESSAGE_INVALID_ARGUMENT, "Display");
+			}
+		}
+		
+		
+		if(!indicMsg.isTrue()) {
+			return indicMsg.getMessage();
+		}
+		
+		if(displayTasksList.isEmpty()) {
+			return MessageList.MESSAGE_NO_TASK_IN_DISPLAY_LIST;
+		}
+		
+		return displayTaskDetails(displayTasksList);
+	}
+	
+	private static IndicatorMessagePair displaySchedule(HashMap<String, String> keyFieldsList, Data smtData, ArrayList<Task> displayTasksList) {
+		
+		if(!keyFieldsList.get(KeywordType.List_Keywords.SCHEDULE.name()).isEmpty()) {
+			return new IndicatorMessagePair(false, MessageList.MESSAGE_INVALID_ARGUMENT);
+		}
+		for(int i = 0; i < smtData.getSize(); i++) {
+			displayTasksList.add(smtData.getATask(i));		
+		}
+		return new IndicatorMessagePair(true, "Success");
+	}
+		
+	private static IndicatorMessagePair displayTodayTasks(HashMap<String, String> keyFieldsList, Data smtData, ArrayList<Task> displayTasksList) {
+		
+		if(!keyFieldsList.get(KeywordType.List_Keywords.TODAY.name()).isEmpty()) {
+			return new IndicatorMessagePair(false, MessageList.MESSAGE_INVALID_ARGUMENT);
+		}
+	
+		LocalDate date = new LocalDate();
+		DateTime endDate = DateParser.generateDate(date.toString());
+		
+		for(int i = 0; i < smtData.getSize(); i++) {
+			if(smtData.getATask(i).getTaskEndDateTime() != null) {
+				if(smtData.getATask(i).getTaskEndDateTime().toLocalDate().equals(endDate.toLocalDate())) {
+				displayTasksList.add(smtData.getATask(i));	
+				}
+			}
+		}
+		return new IndicatorMessagePair(true, "Success");
+	}
+	
+	private static IndicatorMessagePair displayCompletedTasks(HashMap<String, String> keyFieldsList, Data smtData, ArrayList<Task> displayTasksList) {
+		
+		if(!keyFieldsList.get(KeywordType.List_Keywords.TODO.name()).isEmpty()) {	
+			return new IndicatorMessagePair(false, MessageList.MESSAGE_INVALID_ARGUMENT);
+		}
+		
+		for(int i = 0; i < smtData.getSize(); i++) {
+			if(smtData.getATask(i).getTaskStatus()) {
+				displayTasksList.add(smtData.getATask(i));		
+			}
+		}
+		return new IndicatorMessagePair(true, "Success");
+	}
+	
+	private static IndicatorMessagePair displayNotCompletedTasks(HashMap<String, String> keyFieldsList, Data smtData, ArrayList<Task> displayTasksList) {
+		
+		if(!keyFieldsList.get(KeywordType.List_Keywords.PENDING.name()).isEmpty()) {
+			return new IndicatorMessagePair(false, MessageList.MESSAGE_INVALID_ARGUMENT);
+		}
+		
+		for(int i = 0; i < smtData.getSize(); i++) {
+			if(!smtData.getATask(i).getTaskStatus()) {
+				displayTasksList.add(smtData.getATask(i));		
+			}
+		}
+		return new IndicatorMessagePair(true, "Success");
+	}
+	
+}
---
+++ src/logic/DisplayHandlerTest.java
@@ -0,0 +1,87 @@
+package logic;
+import static org.junit.Assert.*;
+
+
+import java.util.HashMap;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import parser.DateParser;
+import storage.FileStorage;
+import data.Data;
+import data.Task;
+
+
+public class DisplayHandlerTest {
+
+	HashMap<String, String> keyFieldsTest;
+	Data smtDataTest;
+	String fileName = "taskList.txt";
+	
+	LocalDate date = new LocalDate();
+	DateTime end = DateParser.generateDate(date.toString());
+	String today = DateParser.displayDateTime(end);
+	
+	@Before
+	public void setUp() throws Exception {
+		smtDataTest = new Data();
+		FileStorage.setFileNameForTasksList(fileName);
+		DateTime startDate = DateParser.generateDate("12/3/2015");
+		DateTime endDate = DateParser.generateDate("25/1/2015");
+		keyFieldsTest = new HashMap<String, String>();
+
+		smtDataTest.addATaskToList(new Task(1, "CE1", startDate, endDate, true, ""));
+		smtDataTest.addATaskToList(new Task(2, "CE2", startDate, endDate, true, ""));
+		smtDataTest.addATaskToList(new Task(3, "V5.0", startDate, new DateTime(), false, ""));
+		smtDataTest.addATaskToList(new Task(4, "Proj Demo", startDate, endDate, false, ""));
+		smtDataTest.addATaskToList(new Task(5, "Proj Video", startDate, endDate, false, ""));
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+		keyFieldsTest.clear();
+	}
+	
+	@Test
+	public void testDisplaySchedule() {
+		
+		keyFieldsTest.put("DISPLAY", "");
+		keyFieldsTest.put("SCHEDULE", "");
+		
+		String expected = "\nTask ID: 1\nDescription: CE1\nStart from: 12 March, 2015\nDeadline: 25 January, 2015\nStatus: Completed\n\nTask ID: 2\nDescription: CE2\nStart from: 12 March, 2015\nDeadline: 25 January, 2015\nStatus: Completed\n\nTask ID: 3\nDescription: V5.0\nStart from: 12 March, 2015\nDeadline: "+ today +"\nStatus: Pending\n\nTask ID: 4\nDescription: Proj Demo\nStart from: 12 March, 2015\nDeadline: 25 January, 2015\nStatus: Pending\n\nTask ID: 5\nDescription: Proj Video\nStart from: 12 March, 2015\nDeadline: 25 January, 2015\nStatus: Pending\n";
+		assertEquals(expected, DisplayHandler.executeDisplay(keyFieldsTest, smtDataTest));
+	}
+	
+	@Test
+	public void testDisplayTodayTasks() {
+		
+		keyFieldsTest.put("DISPLAY", "");
+		keyFieldsTest.put("TODAY", "");
+		
+		String expected = "\nTask ID: 3\nDescription: V5.0\nStart from: 12 March, 2015\nDeadline: "+ today + "\nStatus: Pending\n";
+		assertEquals(expected, DisplayHandler.executeDisplay(keyFieldsTest, smtDataTest));
+	}
+
+	@Test
+	public void testDisplayCompletedTasks() {
+		
+		keyFieldsTest.put("DISPLAY", "");
+		keyFieldsTest.put("TODO", "");
+		
+		String expected = "\nTask ID: 1\nDescription: CE1\nStart from: 12 March, 2015\nDeadline: 25 January, 2015\nStatus: Completed\n\nTask ID: 2\nDescription: CE2\nStart from: 12 March, 2015\nDeadline: 25 January, 2015\nStatus: Completed\n";
+		assertEquals(expected, DisplayHandler.executeDisplay(keyFieldsTest, smtDataTest));
+	}
+	
+	@Test
+	public void testDisplayNotCompletedTasks() {
+		
+		keyFieldsTest.put("DISPLAY", "");
+		keyFieldsTest.put("PENDING", "");
+		
+		String expected = "\nTask ID: 3\nDescription: V5.0\nStart from: 12 March, 2015\nDeadline: "+ today + "\nStatus: Pending\n\nTask ID: 4\nDescription: Proj Demo\nStart from: 12 March, 2015\nDeadline: 25 January, 2015\nStatus: Pending\n\nTask ID: 5\nDescription: Proj Video\nStart from: 12 March, 2015\nDeadline: 25 January, 2015\nStatus: Pending\n";
+		assertEquals(expected, DisplayHandler.executeDisplay(keyFieldsTest, smtDataTest));
+	}
+}
