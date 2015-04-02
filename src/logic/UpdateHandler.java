5f634b74bb4d4b5cde510f73242f12814a08fd9c
TS <ts_neo89@hotmail.com>
2015-04-02 10:24:33 +0800

    Update handler changed taskstart and taskend into "from" and  "to" keyword

--- src/logic/UpdateHandler.java
+++ src/logic/UpdateHandler.java
@@ -64,8 +64,18 @@ public class UpdateHandler {
 	 * @return message
 	 */
 	private static String updateContents(HashMap<String, String> keyFieldsList, int index, Data smtData){
-		IndicatorMessagePair indicMsg;
+		IndicatorMessagePair indicMsg = new IndicatorMessagePair();
+		indicMsg.setTrue(true);
 		KeywordType.List_Keywords getKey;
+		
+		if(checkFromTimeToTimeBothExist(keyFieldsList)){
+			indicMsg = determineBothTimes(keyFieldsList);
+		}
+		
+		if(indicMsg != null && !indicMsg.isTrue()){
+				return indicMsg.getMessage();
+		}
+		
 		for (String key : keyFieldsList.keySet()) {
 			getKey = KeywordType.getKeyword(key);
 			switch(getKey){
@@ -75,11 +85,11 @@ public class UpdateHandler {
 			case TASKDESC:
 				indicMsg = updateTaskDesc(smtData, index, keyFieldsList.get(key));
 				break;
-			case TASKSTART:
-				indicMsg = updateTaskStartWhen(smtData, index, keyFieldsList.get(key));
+			case FROM:
+				indicMsg = updateTaskFromOrToTimeWhen(smtData, index, keyFieldsList.get(key), KeywordType.List_Keywords.FROM);
 				break;
-			case TASKEND:
-				indicMsg = updateTaskByOrEndWhen(smtData, index, keyFieldsList.get(key));
+			case TO:
+				indicMsg = updateTaskFromOrToTimeWhen(smtData, index, keyFieldsList.get(key), KeywordType.List_Keywords.TO);
 				break;
 			case EVERY:
 				indicMsg = updateRecurringWeek(smtData, index, keyFieldsList.get(key));
@@ -191,7 +201,7 @@ public class UpdateHandler {
 	 * @param keyFields contains the keyword and the data it has
 	 * @return true if success, false if there is an invalid conversion object and message
 	 */
-	private static IndicatorMessagePair updateTaskStartWhen(Data smtData, int index, String keyFields){
+	private static IndicatorMessagePair updateTaskFromOrToTimeWhen(Data smtData, int index, String keyFields, KeywordType.List_Keywords direction){
 		if(smtData == null){
 			assert false : "System error";
 		}
@@ -200,12 +210,21 @@ public class UpdateHandler {
 			return new IndicatorMessagePair(false, MessageList.MESSAGE_NO_DATE_GIVEN);
 		}
 
-		DateTime startDate = DateTimeParser.generateDate(keyFields);
-		if(startDate == null){
+		DateTime startOrEndTime = DateTimeParser.generateTime(keyFields);
+		if(startOrEndTime == null){
 			return new IndicatorMessagePair(false, String.format(MessageList.MESSAGE_WRONG_DATE_FORMAT, "Start"));
 		}
+		
 		Task tempTask = smtData.getATask(index);
-		tempTask.setTaskStartDateTime(startDate);
+		
+		if(direction == KeywordType.List_Keywords.FROM){
+			tempTask.setTaskStartDateTime(updateTimeToExistingDateTime(smtData.getATask(index), startOrEndTime));
+		}
+		else if(direction == KeywordType.List_Keywords.TO){
+			tempTask.setTaskEndDateTime(updateTimeToExistingDateTime(smtData.getATask(index), startOrEndTime));
+		}
+		
+		//tempTask.setTaskStartDateTime(startTime);
 		smtData.updateTaskList(index, tempTask);
 		return new IndicatorMessagePair(true, "");
 	}
@@ -246,6 +265,65 @@ public class UpdateHandler {
 			}
 		}
 		return -1;
+	}
+	
+	private static IndicatorMessagePair determineBothTimes(HashMap<String, String> keyFieldsList){
+		
+		if(!checkFromTimeToTimeBothField(keyFieldsList)){
+			return new IndicatorMessagePair(false, "Time is not entered correctly");
+		}
+		
+		if(!checkFromTimeToTimeBothValid(keyFieldsList)){
+			return new IndicatorMessagePair(false, "Start Time is before End Time");
+		}
+		
+		return new IndicatorMessagePair(true, "");
+	}
+	
+	private static boolean checkFromTimeToTimeBothExist(HashMap<String, String> keyFieldsList){
+		if(keyFieldsList.containsKey(KeywordType.List_Keywords.FROM.name()) && keyFieldsList.containsKey(KeywordType.List_Keywords.TO.name())){
+			return true;
+		}
+		return false;
+	}
+	
+	private static boolean checkFromTimeToTimeBothField(HashMap<String, String> keyFieldsList){
+		if(keyFieldsList.get(KeywordType.List_Keywords.FROM.name()) != null && keyFieldsList.get(KeywordType.List_Keywords.FROM.name()).isEmpty() && keyFieldsList.get(KeywordType.List_Keywords.TO.name()) != null && keyFieldsList.get(KeywordType.List_Keywords.TO.name()).isEmpty()){
+			return false;
+		}
+		return true;
+	}
+	
+	private static boolean checkFromTimeToTimeBothValid(HashMap<String, String> keyFieldsList){
+		DateTime startTime = DateTimeParser.generateTime(keyFieldsList.get(KeywordType.List_Keywords.FROM.name()));
+		if(startTime == null){
+			return false;
+		}
+		
+		DateTime endTime = DateTimeParser.generateTime(keyFieldsList.get(KeywordType.List_Keywords.TO.name()));
+		if(endTime == null){
+			return false;
+		}
+		
+		if(startTime.isAfter(endTime)){
+			return false;
+		}
+		
+		return true;
+	}
+	
+	private static DateTime updateTimeToExistingDateTime(Task currentTask, DateTime existingDateTime){
+		if(currentTask.getTaskStartDateTime() == null && currentTask.getTaskEndDateTime() == null){
+			//do nothing
+		}
+		else if(currentTask.getTaskStartDateTime() == null){
+			existingDateTime = new DateTime(currentTask.getTaskEndDateTime().getYear(), currentTask.getTaskEndDateTime().getMonthOfYear(), currentTask.getTaskEndDateTime().getDayOfMonth(), existingDateTime.getHourOfDay(), existingDateTime.getMinuteOfHour());
+		}
+		else{
+			existingDateTime = new DateTime(currentTask.getTaskStartDateTime().getYear(), currentTask.getTaskStartDateTime().getMonthOfYear(), currentTask.getTaskStartDateTime().getDayOfMonth(), existingDateTime.getHourOfDay(), existingDateTime.getMinuteOfHour());
+		}
+		
+		return existingDateTime;
 	}
 	
 	/**
