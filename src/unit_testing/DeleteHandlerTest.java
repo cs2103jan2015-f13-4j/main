03fa9c23a1058b91c6904b0f62452fda82522fbd
Shuna <shunaa.lim@hotmail.com>
2015-04-11 19:36:36 +0800

    Amendments made to DeleteHandlerTest class

--- src/unit_testing/DeleteHandlerTest.java
+++ src/unit_testing/DeleteHandlerTest.java
@@ -12,6 +12,7 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import parser.DateTimeParser;
 import storage.FileStorage;
 import utility.MessageList;
 import data.Data;
@@ -33,12 +34,14 @@ public class DeleteHandlerTest {
 	public void setUp() {
 		smtDataTest = new Data();
 		FileStorage.setFileNameForTasksList(fileName);
+		DateTime startDate = DateTimeParser.generateDate("30/8/2015");
+		DateTime endDate = DateTimeParser.generateDate("30/8/2015");
 		keyFieldsTest = new TreeMap<String, String>();
 		taskList = new ArrayList<Task>();
 		
-		smtDataTest.addATaskToList(new Task(1, "Prepare a proposal", new DateTime(), new DateTime(), ""));
-		smtDataTest.addATaskToList(new Task(2, "Submit report to Ms Sarah", new DateTime(), new DateTime(), ""));
-		smtDataTest.addATaskToList(new Task(3, "Prepare OP1", new DateTime(), new DateTime(), ""));
+		smtDataTest.addATaskToList(new Task(1, "Prepare a proposal", startDate, endDate, ""));
+		smtDataTest.addATaskToList(new Task(2, "Submit report to Ms Sarah", null, null, ""));
+		smtDataTest.addATaskToList(new Task(3, "Prepare OP1", startDate, endDate, ""));
 	}
 
 	@After
@@ -51,7 +54,7 @@ public class DeleteHandlerTest {
 	@Test
 	public void testDeleteWithIDRegular() {
 		keyFieldsTest.put("DELETE", "2");
-		String expected = String.format(MessageList.MESSAGE_DELETE_SUCCESS, fileName, "Submit report to Ms Sarah");
+		String expected = String.format("\nTask ID: 2\nDescription: Submit report to Ms Sarah\nStatus: Pending\nDeleted");
 		assertEquals(expected, DeleteHandler.executeDelete(keyFieldsTest, smtDataTest));
 	}
 	
