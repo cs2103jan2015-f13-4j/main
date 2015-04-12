c776dd0d171b8b70e1df70a34430f619cd0a6a60
Shuna <shunaa.lim@hotmail.com>
2015-04-12 13:02:23 +0800

    Amendments made to CommandType class

--- src/utility/CommandType.java
+++ src/utility/CommandType.java
@@ -3,7 +3,7 @@ package utility;
 
 /**
  * This class is use to indicate the command types
- *
+ * 
  */
 
 public class CommandType {
@@ -11,12 +11,15 @@ public class CommandType {
 		ADD, DELETE, DISPLAY, EXIT, UPDATE, INVALID, SEARCH, SORT, HELP, UNDO, REDO, BLOCK, UNBLOCK
 	}
 
-	
-	//@author A0112502A
+	// @author A0112502A
 	/**
-	 * This method will check the commands input by the user and go to the necessary case
-	 * @param commandTypeString is the command type that user has input 
-	 * @return the corresponding command type with the commandTypeString received
+	 * This method will check the commands input by the user and go to the
+	 * necessary case
+	 * 
+	 * @param commandTypeString
+	 *            is the command type that user has input
+	 * @return the corresponding command type with the commandTypeString
+	 *         received
 	 */
 	public static Command_Types getType(String[] commandTypeString) {
 
@@ -24,44 +27,57 @@ public class CommandType {
 			assert false : "Invalid command types entered";
 			return Command_Types.INVALID;
 		}
-		
+
 		switch (commandTypeString[0].toLowerCase()) {
 		case "add":
-		case "a":
+		case "a": {
 			return Command_Types.ADD;
-		case "delete": 
-		case "del":
+		}
+		case "delete":
+		case "del": {
 			return Command_Types.DELETE;
-		case "display": 
-		case "dis":
+		}
+		case "display":
+		case "dis": {
 			return Command_Types.DISPLAY;
-		case "update": 
-		case "up":
+		}
+		case "update":
+		case "up": {
 			return Command_Types.UPDATE;
-		case "exit":
+		}
+		case "exit": {
 			return Command_Types.EXIT;
+		}
 		case "search":
-		case "sh":
+		case "sh": {
 			return Command_Types.SEARCH;
-		case "sort": 
-		case "st":
+		}
+		case "sort":
+		case "st": {
 			return Command_Types.SORT;
-		case "help":
+		}
+		case "help": {
 			return Command_Types.HELP;
+		}
 		case "undo":
-		case "ud":
+		case "ud": {
 			return Command_Types.UNDO;
+		}
 		case "redo":
-		case "rd":
+		case "rd": {
 			return Command_Types.REDO;
+		}
 		case "block":
-		case "b": 
+		case "b": {
 			return Command_Types.BLOCK;
+		}
 		case "unblock":
-		case "ub": 
+		case "ub": {
 			return Command_Types.UNBLOCK;
-		default: 
+		}
+		default: {
 			return Command_Types.INVALID;
+		}
 		}
 	}
 }
