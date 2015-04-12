ac268a1b0dabe594c7041d14733ea47c799bee7d
wq <wq@project.com>
2015-04-12 08:43:16 +0800

    Updated author.

--- src/utility/TaskLogging.java
+++ src/utility/TaskLogging.java
@@ -1,10 +1,9 @@
-//@A0112978W
+//@author A0112978W
 package utility;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
-
 
 public class TaskLogging {
 	public static Logger logger;
